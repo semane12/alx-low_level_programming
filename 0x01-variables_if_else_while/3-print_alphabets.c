27b28505f12e379148d88346452a74120d73efb7
semane12 <114406855+semane12@users.noreply.github.com>
2022-10-14 22:48:44 +0300

    Update 3-print_alphabets.c

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -9,8 +9,10 @@
 int main(void)
 {
 	char ch;
-	for (ch = 'a''A'; ch <= 'z''Z'; ch++)
-	
+	for (ch = 'a'; ch <= 'z'; ch++)
+	{
+		for (ch = 'a'; ch <= 'z'; ch++)
+		}	
 		putchar(ch);
 
 	return (0);
