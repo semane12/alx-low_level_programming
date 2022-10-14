5ab08eb7dc16380b258506bd9e604712bc5b04ff
semane12 <semanes2012@gmail.com>
2022-10-14 02:08:49 -0700

    1. last digit

---
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - print last digit of
+ *Return: 0
+ */
+int main(void)
+{
+	int n;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	printf("%d Last digit of\n", n);
+
+	retutn (0)
+}
