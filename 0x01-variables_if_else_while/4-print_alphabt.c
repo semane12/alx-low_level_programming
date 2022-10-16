61e389b2ff50b575edeeab7d5e3a6703be2b9cbb
semane12 <semanes2012@gmail.com>
2022-10-16 09:18:01 -0700

    4. print alphabt

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -3,13 +3,13 @@
 /**
  * main - print alphabt
  *
- * Return: 0
+ * Return: Always 0
  */
 int main(void)
 {
 	char ch;
 
-	for (ch = 'a'; ch = 'z'; ch++)
+	for (ch = 'a'; ch <= 'z'; ch++)
 	{
 		if (ch != 'e' && ch != 'q')
 			putchar(ch);
