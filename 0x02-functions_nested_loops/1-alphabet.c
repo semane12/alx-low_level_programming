73ad4e9fd20d1d1cc3f68e382ff5d10648256dbe
semane12 <semanes2012@gmail.com>
2022-10-18 12:11:16 -0700

    1 alphabet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * main - print alphabet in lowercase
+ */
+void print_alphabet(void)
+{
+	char ch;
+
+	for (ch = 'a'; ch <= 'z'; ch++)
+		_putchar(ch);
+
+	-putchar('\n');
+}
