823cee5911d755268d6125588919f2f557a23544
semane12 <semanes2012@gmail.com>
2022-10-18 12:21:11 -0700

    2. print alphaber x10

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - print alphabet 10 times
+ */
+void print_alphabet_x10(void)
+{int ten;
+	char ch;
+
+	for (ten = 0; ten <= 9; ten++)
+	{
+		for (ch = 'a'; ch <= 'z'; ch++)
+			_putchar(ch);
+		_putchar('\n');
+	}
+}
