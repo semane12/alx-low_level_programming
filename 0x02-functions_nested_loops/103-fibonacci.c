9ea3daa84a1a25b6dd368953c94edd4d119a31e3
semane12 <semanes2012@gmail.com>
2022-10-19 11:49:55 -0700

    103. fibonancy

---
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - display fibonancy number
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	int i;
+	int n;
+	for (n > 0; n = (i -1) + (i + 1); n++)
+		putchar(n);
+	return (0);
+}
