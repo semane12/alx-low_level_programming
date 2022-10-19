14f22b5b8dd37b7fa1fd8fc8d179e1e1e0a98094
semane12 <semanes2012@gmail.com>
2022-10-19 01:06:42 -0700

    0. main

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * main - tests function and print if intiger is positive or negative
+ * Return: 0
+ */
+int main(void)
+{
+	int i;
+
+	i = 0;
+	positive_or_negative(i);
+
+	return (0);
+}
