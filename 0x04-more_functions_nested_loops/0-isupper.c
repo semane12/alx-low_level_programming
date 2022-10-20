ed60d497c6be369c5889b30796adb39c1b5b1c01
semane12 <semanes2012@gmail.com>
2022-10-20 01:27:40 -0700

    0. isupper

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * _isupper(int c) - check for uppercase character
+ * Return: 0
+ */
+int _isupper(int c);
+{
+	int c;
+	for (c = 'A' && c <= 'Z')
+		putchar(c);
+	return (0);
+}
