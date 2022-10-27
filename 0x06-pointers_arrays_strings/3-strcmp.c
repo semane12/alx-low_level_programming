bd130a91432ffc54df0bf3feafe598e12462c364
semane12 <semanes2012@gmail.com>
2022-10-27 06:56:06 -0700

    3. strcmp

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * _strcmd - compares two strings values
+ * _strcmp - compares two strings values
  * @s1: string 1
  * @s2: string 2
  * Return: the difference in s1 and s2
