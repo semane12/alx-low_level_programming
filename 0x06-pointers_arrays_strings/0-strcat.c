18fc7dc440b451ee9497be40fa5460b64752dbed
semane12 <semanes2012@gmail.com>
2022-10-27 05:56:26 -0700

    0. strcat

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -10,14 +10,13 @@ char *_strcat(char *dest, char *src)
 {
 	int i;
 	int j;
-	
 	i = 0;
 	while (dest[i] != '\0')
 	{
 		i++;
 	}
 	j = 0;
-	while (src[j] !='\0')
+	while (src[j] != '\0')
 	{
 		dest[i] = src[j];
 		i++;
