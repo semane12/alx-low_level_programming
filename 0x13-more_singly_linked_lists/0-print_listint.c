d24e45b84ec8691a891a98883fb5d149a98f7152
semane12 <semanes2012@gmail.com>
2022-11-28 07:56:12 -0800

    0. print listint

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -1,22 +1,24 @@
-#include "lists.h"
+#include "lists.h"                                                                 
+                                                                                  
 
-/**
- * print_listint - prints all the elements of a linked list
+/**                                                                                
+ * print_listint - prints all the elements of a linked list                        
  * @h: head of the list
  *
- * Return: the number of nodes
- */
+ * Return: the number of nodes                                                     
+ */                                                                             
+                                                                                  
+size_t print_listint(const listint_t *h)                                           
+{                                                                                  
+        const listint_t *cursor = h;                                               
+        size_t count = 0;                                                                                                                                           
+        while (cursor != NULL)                                                     
+        {                                                                          
+                printf("%d\n", cursor->n);                                         
+	        count += 1;                                                        
+	        cursor = cursor->next;                                             
+        }                                                                          
 
-size_t print_listint(const listint_t *h)
-{
-	const listint_t *cursor = h;
-	size_t count = 0;
+	return (count);                                                            
 
-	while (cursor != NULL)
-	{
-		printf("%d\n", cursor->n);
-		count += 1;
-		cursor = cursor->next;
-	}
-	return (count);
 }
