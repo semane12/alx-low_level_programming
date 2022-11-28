7963fa27967e90f24c59f68fa3839faa51a174f2
semane12 <114406855+semane12@users.noreply.github.com>
2022-11-28 08:03:11 -0800

    0. print listint

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -3,8 +3,10 @@
 /**
  * print_listint - prints all the elements of a linked list
  * @h: head of the list
+ *
  * Return: the number of nodes
  */
+
 size_t print_listint(const listint_t *h)
 {
 	const listint_t *cursor = h;
