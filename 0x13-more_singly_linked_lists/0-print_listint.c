0f84b49dc89ce56f7b087a93bd1977bdb1b3875d
semane12 <114406855+semane12@users.noreply.github.com>
2022-11-29 04:26:59 -0800

    0. print listint

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -8,14 +8,12 @@
  */
 size_t print_listint(const listint_t *h)
 {
-	const listint_t *cursor = h;
 	size_t count = 0;
 
-	while (cursor != NULL)
+	while (h)
 	{
-		printf("%d\n", cursor->n);
-		count += 1;
-		cursor = cursor->next;
+		count++;
+		h = h->next;
 	}
 	return (count);
 }
