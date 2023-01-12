4dffd26c10cf0d0c9950af0079f05164431ec956
semane12 <114406855+semane12@users.noreply.github.com>
2023-01-12 13:15:36 +0300

    0. print dlistint

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -1,27 +1,30 @@
-#include <stdlib.h>
-#include <string.h>
-#include <stdio.h>
 #include "lists.h"
+
 /**
- * * main - check the code
- * *
- * * Return: Always EXIT_SUCCESS.
- * */
-int main(void)
+ * print_dlistint - prints all the elements of a
+ * dlistint_t list
+ *
+ * @h: head of the list
+ * Return: the number of nodes
+ */
+size_t print_dlistint(const dlistint_t *h)
 {
-	dlistint_t *head; dlistint_t *new;
-	dlistint_t hello = {8, NULL, NULL};
-	size_t n; head = &hello; new = malloc(sizeof(dlistint_t));
-	if (new == NULL)
+	int count;
+
+	count = 0;
+
+	if (h == NULL)
+		return (count);
+
+	while (h->prev != NULL)
+		h = h->prev;
+
+	while (h != NULL)
 	{
-		dprintf(2, "Error: Can't malloc\n");
-		return (EXIT_FAILURE);
+		printf("%d\n", h->n);
+		count++;
+		h = h->next;
 	}
-	new->n = 9;
-	head->prev = new;
-	new->next = head;
-	new->prev = NULL;
-	head = new; n = print_dlistint(head); printf("-> %lu elements\n", n);
-	free(new);
-	return (EXIT_SUCCESS);
+
+	return (count);
 }
